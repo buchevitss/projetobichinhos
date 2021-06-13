819f7c75b1c9a1b5d58510db824984b88cf14d87
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-06-13 11:23:51 -0300

    Att 1.0.4

---
+++ Doacao.java
@@ -0,0 +1,56 @@
+package projetobichinho;
+
+import java.util.Scanner;
+
+public class Doacao {
+	
+	private String nDoador;
+	private String tf;
+	private int vDoado;
+	
+      public Doacao(String nDoador,String tf,int pDoacao,int vDoado)
+       {
+	     this.nDoador = nDoador;
+	     this.tf = tf;
+	     this.vDoado = vDoado;
+       }
+      
+      Scanner scan = new Scanner(System.in);
+      	  
+      public void printD()
+      {
+    	    System.out.println("\nPara doar ração vamos um simples cadastro!!\nDigite seu nome completo:");
+    	    nDoador = scan.nextLine();
+    	    System.out.println("\nAgora informe seu telefone: ");
+    	    tf = scan.nextLine();
+    	    System.out.println("\nQual valor voce deseja doar: R$");
+    	    vDoado = scan.nextInt();
+    	    System.out.println("\nNome doador: "+nDoador+"\nTelefone: "+tf+"\nValor doado: "+vDoado);
+      }
+
+	public String getnDoador() {
+		return nDoador;
+	}
+
+	public void setnDoador(String nDoador) {
+		this.nDoador = nDoador;
+	}
+
+	public String getTf() {
+		return tf;
+	}
+
+	public void setTf(String tf) {
+		this.tf = tf;
+	}
+
+	public int getvDoado() {
+		return vDoado;
+	}
+
+	public void setvDoado(int vDoado) {
+		this.vDoado = vDoado;
+	}
+
+      
+}
