08781ac8d5a60cefe5bca0516766ed975d53a6a5
AmandaBritoPereira <85176263+AmandaBritoPereira@users.noreply.github.com>
2021-06-13 19:43:03 -0300

    versão horário 19:42

--- Doacao.java
+++ Doacao.java
@@ -3,37 +3,29 @@ package Generation;
 import java.util.Scanner;
 
 public class Doacao {
-	int y;
-	String [] nDoador = new String [y];
+	String y;
+	String nDoador;
 	String dado;
 	private String tf;
 	private int vDoado;
-	
-      public Doacao(String tf,int vDoado)
-       {
-	     this.tf = tf;
-	     this.vDoado = vDoado;
-       }
-      
-      Scanner scan = new Scanner(System.in);
-      	 
-      public void preecher () {
-    	
-    	  if ( clicado1 ) {
-    		y++;
-    		System.out.println("\nPara doar ração vamos fazer um simples cadastro!!\nDigite seu nome completo:");
-    		nDoador [y] = scan.nextLine();
-    	  }
-      }
-      public void printD()
-      {
-    	    
-    	    System.out.println("\nAgora informe seu telefone: ");
-    	    tf = scan.nextLine();
-    	    System.out.println("\nQual valor voce deseja doar: R$");
-    	    vDoado = scan.nextInt();
-    	    System.out.println("\nNome doador: "+nDoador+"\nTelefone: "+tf+"\nValor doado: "+vDoado);
-      }
+
+	public Doacao(String nDoador, String tf, int vDoado) {
+		this.nDoador = nDoador;
+		this.tf = tf;
+		this.vDoado = vDoado;
+	}
+
+	Scanner scan = new Scanner(System.in);
+
+	public void printD() {
+		System.out.println("\nPara doar ração vamos fazer um simples cadastro!!\nDigite seu nome completo:");
+		nDoador = scan.nextLine();
+		System.out.println("\nAgora informe seu telefone: ");
+		tf = scan.nextLine();
+		System.out.println("\nQual valor voce deseja doar: R$");
+		vDoado = scan.nextInt();
+		System.out.println("\nNome doador: " + nDoador + "\nTelefone: " + tf + "\nValor doado: " + vDoado);
+	}
 
 	public String getnDoador() {
 		return nDoador;
@@ -59,5 +51,4 @@ public class Doacao {
 		this.vDoado = vDoado;
 	}
 
-      
 }
--- TesteAnimal.java
+++ TesteAnimal.java
@@ -1,4 +1,4 @@
-package projetobichinho;
+package Generation;
 
 import java.util.Scanner;
 
@@ -11,18 +11,20 @@ public class TesteAnimal {
 		int s;
 		int sx = 0;
 		System.out.println(
-				"\nQual Animal voce esta pesquisando, clique no número correspondente: \n1 - Cachorro\n2 - Gato\n3 - Animais Silvestres\n0-Para encerrar");
+				"\nQual Animal voce esta pesquisando, clique no número correspondente: \n1 - Cachorro\n2 - Gato\n3 - Animais Silvestres\n4 - Doar valores para compra de ração\n0-Para encerrar");
 		s = scan.nextInt();
 
 		while (s != 0) {
 			switch (s) {
 			case 1:
 				System.out.println("Digite o nome do cachorro: ");
-				//String nome = scan.next();
+				// String nome = scan.next();
 
-				/*cachorro cao1 = new cachorro("dog", 1, "LhasaApso", "Grande", "OI", "Preto", "SemDoença", "Vacinado ",
-						"Castrado", "Calmo", "RGrau", 11945, "Macho");
-				cao1.imprimirInfo();*/
+				/*
+				 * cachorro cao1 = new cachorro("dog", 1, "LhasaApso", "Grande", "OI", "Preto",
+				 * "SemDoença", "Vacinado ", "Castrado", "Calmo", "RGrau", 11945, "Macho");
+				 * cao1.imprimirInfo();
+				 */
 
 				break;
 			case 2:
@@ -36,46 +38,69 @@ public class TesteAnimal {
 				break;
 			case 3:
 
-			do {
+				do {
 
-				System.out.println("\nDigite o sexo do pet q vc procura: \n1-Macho\n2Fêmea");
-				sx = scan.nextInt();
+					System.out.println("\nDigite o sexo do pet q vc procura: \n1-Macho\n2Fêmea");
+					sx = scan.nextInt();
 
-				if (sx == 1) {
-					System.out.println ("\nEsses são nossos animais silvestres machos: ");
-					
-					AnimaisSilvestres Aves1 = new AnimaisSilvestres("Gunter", 3, "preta", "vacinado",  "castrado", "bonzinho",
-							 "Rua Capixaba", 959129749, "macho", "ave", "pequeno");
-					Aves1.imprimirInfo();
-					System.out.println("\n======================================================================================\n");
-						AnimaisSilvestres Jabuti1 = new AnimaisSilvestres("Jamelão",8,"cinza","vacinada","Não castrada","dócil",
-								"Rua da concórdia",545514,"Macho","Jabuti","Pequeno");
+					if (sx == 1) {
+						System.out.println("\nEsses são nossos animais silvestres machos: ");
+
+						AnimaisSilvestres Aves1 = new AnimaisSilvestres("Gunter", 3, "preta", "vacinado", "castrado",
+								"bonzinho", "Rua Capixaba", 959129749, "macho", "ave", "pequeno");
+						Aves1.imprimirInfo();
+						System.out.println(
+								"\n======================================================================================\n");
+						AnimaisSilvestres Jabuti1 = new AnimaisSilvestres("Jamelão", 8, "cinza", "vacinada",
+								"Não castrada", "dócil", "Rua da concórdia", 545514, "Macho", "Jabuti", "Pequeno");
 						Jabuti1.imprimirInfo();
-					
-					
 
-				} else if (sx == 2) {
-					System.out.println("\nEsses são nossos animais silvestres fêmeas: ");
-					
-					AnimaisSilvestres Papagaio1 = new AnimaisSilvestres ("Lora",2,"verde","vacinada","não castrada.","dócil",
-							"Rua Bahia",119343454,"Fêmea","Ave","Pequeno");
-					Papagaio1.imprimirInfo();
-					System.out.println("\n======================================================================================\n");
-					
-					AnimaisSilvestres Cobra1 = new AnimaisSilvestres("Gertrude",15,"cinza","vacinada","Não castrada","dócil",
-							"Rua da gruta",54543234,"Fêmea","Réptil(cobra)","Médio");
-					Cobra1.imprimirInfo();
-					
-				}
-				
-			}while (sx != 1 && sx != 2);
-			
-				//Coloquei um número diferente de 0, para que saia do primeiro laço de repetição while, que informava != 0 como condição.
-				System.out.println ("\nPara finalizar, clique em 0:");
-				s = scan.nextInt ();
-				
+					} else if (sx == 2) {
+						System.out.println("\nEsses são nossos animais silvestres fêmeas: ");
+
+						AnimaisSilvestres Papagaio1 = new AnimaisSilvestres("Lora", 2, "verde", "vacinada",
+								"não castrada", "dócil", "Rua Bahia", 119343454, "Fêmea", "Ave", "Pequeno");
+						Papagaio1.imprimirInfo();
+						System.out.println(
+								"\n======================================================================================\n");
+
+						AnimaisSilvestres Cobra1 = new AnimaisSilvestres("Gertrude", 15, "cinza", "vacinada",
+								"Não castrada", "dócil", "Rua da gruta", 54543234, "Fêmea", "Réptil(cobra)", "Médio");
+						Cobra1.imprimirInfo();
+
+					}
+
+				} while (sx != 1 && sx != 2);
+
+				// Coloquei um número diferente de 0, para que saia do primeiro laço de
+				// repetição while, que informava != 0 como condição.
+				System.out.println("\nPara voltar ao menu, insira um número");
+				s = scan.nextInt();
+
 				break;
-			}
+			case 4:
+
+				Doacao doador = new Doacao("amanda", "959", 11);
+				doador.printD();
+
+				System.out.println("\nOs bichinhos agradecem!");
+				System.out.println("\nPara voltar ao menu, insira um número.");
+				s = scan.nextInt();
+				break; 
+				
+			default:  
+				System.out.println(
+				"\nQual Animal voce esta pesquisando, clique no número correspondente: \n1 - Cachorro\n2 - Gato\n3 - Animais Silvestres\n4 - Doar valores para compra de ração\n0-Para encerrar");
+				s = scan.nextInt();
+			} 
+			
 		}
+		System.out.println("\nOs animaizinhos também recebem visitas sem compromisso.\n"
+				+ "\nJabuti Jamelão: Sou feliz em dizer, que nos lares provisórios somos bem tratados."
+				+ "\nGata Peneloppe: Quando pedimos muito carinho, vem crianças nos ver! "
+				+ "\nCão Thor:E é cada adulto cheio de amor, que me enche de abraços."
+				+ "\n\nGostou de algo? Fale com a gente! Org Hakuna."
+				+ "\nTelefone: 11 98477-0988. End: Rua Bituva, 56. Jd. Capela. São Paulo - SP.");
 	}
+
 }
