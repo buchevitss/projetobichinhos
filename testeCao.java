36f212c240d5652f8f97c0b27c24e3e93712f024
Mara Jozicleia <64552937+Mara-Jozicleia@users.noreply.github.com>
2021-06-13 20:23:45 -0300

    Add files via upload

---
+++ testeCao.java
@@ -0,0 +1,17 @@
+package Projeto_Adocao;
+
+import java.util.Scanner;
+
+public class testeCao {
+
+	public static void main(String[] args) {
+		
+
+		Cao Ong = new Cao();
+        Ong.ficha();
+		}
+		
+
+	}
+
+
