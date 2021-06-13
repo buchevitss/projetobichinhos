3e24cc4467d0b3a32c76d5f35643804af2ecc546
Tainá Pimenta <81496791+TainaPimenta@users.noreply.github.com>
2021-06-13 20:25:38 -0300

    Add files via upload

---
+++ TesteGato.java
@@ -0,0 +1,19 @@
+package projetoBichinhos;
+
+public class TesteGato {
+
+	public static void main(String[] args) {
+		
+		/*String nome, int idade,
+		String cor, String sexo, String doenca, String raca, String porte, String vacina,  String castrado, String comportamento,
+		 String endereco, int telefone*/
+
+		 Gato Ong = new Gato();
+	      Ong.historico();
+	}
+	
+}
+
+
+
+
